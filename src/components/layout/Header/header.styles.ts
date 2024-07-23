e5eb1ecb4d83436948839d06c668691bdce063cc
maaxxon <wmaxwas@gmail.com>
2024-07-23 19:55:39 +0300

    add: footer and header

---
+++ src/components/layout/Header/header.styles.ts
@@ -0,0 +1,73 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const HeaderDiv = styled.header`
+  background: linear-gradient(
+    to right,
+    ${({ theme }) => theme.colors.text},
+    ${({ theme }) => theme.colors.headerGradientEnd}
+  );
+  position: relative;
+`;
+
+export const HeaderWrapper = styled.div`
+  padding: ${({ theme }) => theme.spacing.footerPadding} 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.headerText};
+`;
+
+export const LogoWrapper = styled(Link)`
+  display: flex;
+  align-items: end;
+  gap: ${({ theme }) => theme.spacing.small};
+  text-decoration: none;
+`;
+
+export const IconLogo = styled.div`
+  width: 48px;
+  height: 48px;
+  background-image: url(/logo.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+export const Title = styled.h1`
+  font-size: 1rem;
+  margin: 0;
+  color: ${({ theme }) => theme.colors.headerText};
+  span {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const LinkWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.headerText};
+  text-decoration: none;
+  gap: 1rem;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
+`;
+
+export const FavoritesLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.headerText};
+  text-decoration: none;
+  gap: 0.5rem;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const IconBookmark = styled.img`
+  width: 24px;
+  height: 24px;
+  color: ${({ theme }) => theme.colors.bookmarkIcon};
+`;
