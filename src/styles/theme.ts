0f982abe06490afaa1a0653052be1a82175069d5
maaxxon <wmaxwas@gmail.com>
2024-07-23 19:52:21 +0300

    add: theme vars

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+export const theme = {
+  colors: {
+    primary: '#E0A449',
+    text: '#333',
+    background: '#f5f5f5',
+    backgroundDark: '#252525',
+    footerBackground: 'white',
+    footerText: 'black',
+    footerLink: 'white',
+    footerLinkHover: '#f0a500',
+    headerGradientEnd: '#444',
+    headerText: 'white',
+    bookmarkIcon: '#e0a449',
+    cardBackground: 'white',
+    cardShadow: 'rgba(0, 0, 0, 0.1)',
+    imagePlaceholder: '#e0e0e0',
+    imagePlaceholderText: '#999',
+    cardStatus: '#666',
+    error:'#842029'
+  },
+  fonts: {
+    main: 'Roboto, sans-serif',
+    code: 'source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace',
+  },
+  spacing: {
+    small: '16px',
+    medium: '32px',
+    large: '110px',
+    footerPadding: '1rem',
+  },
+  boxShadow: '0 0.125rem 0.3125rem rgba(0, 0, 0, 0.1)',
+  borderRadius: '0.625rem',
+};
